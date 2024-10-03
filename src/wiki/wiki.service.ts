4ee7fbf796d3aaafa42e466dde181a9031f3226d
Juan José Medina <jjr20060326@gmail.com>
2024-10-03 11:10:47 -0500

    se agrego una pequeña cosita para las img

--- src/wiki/wiki.service.ts
+++ src/wiki/wiki.service.ts
@@ -22,6 +22,7 @@ export class WikiService {
             commonName: true,
             scientificName: true,
             family: true,
+            imageURL: true,
           }
         }
       );
